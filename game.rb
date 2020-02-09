330bee8e7e4c22379786505a1d60c562852a9fb6
Stephen Allard <stephencallard@gmail.com>
2020-02-09 15:12:25 -0600

    Created promp_player method in the Game class, and added a main.rb file to run the game from

--- game.rb
+++ game.rb
@@ -12,11 +12,27 @@ class Game
     '12O45O78O','O234O678O','12O4O6O89']
 
     if winning_combos_x.include?(@board.contents)
-        return 'X'
+      return 'X'
     elsif winning_combos_o.include?(@board.contents)
-        return 'O'
+      return 'O'
     else
-        return nil
+      return nil
     end
   end
+
+  def prompt_player(player)
+    input = 0
+    while(!input)
+      puts "#{player.name}, enter a position (1 - 9), or press q to quit"
+      input = gets.chomp
+      input = 0 unless (1..9).include?(input) || input == 'q'
+    end
+    if input.is_a? Numeric
+      player.make_move(input)
+    else
+      ##quit game
+    end
+  end
+ 
+
 end
---
+++ main.rb
@@ -0,0 +1,6 @@
+require './game.rb'
+require './player.rb' 
+require './game_board.rb'
+
+player1 = Player.new('Stephen','X"')
+puts player1
