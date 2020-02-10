6747b8923251b07bcbe1a526dfc53df990884994
Stephen Allard <stephencallard@gmail.com>
2020-02-09 19:59:30 -0600

    modified winner? method in Game class to use regexps

--- game.rb
+++ game.rb
@@ -11,14 +11,12 @@ class Game
   public
 
   def winner?
-    winning_combos_x = ['XXX456789','123XXX789','123456XXX','X23X56X89','1X34X67X9',
-      '12X45X78X','X234X678X','12X4X6X89']
-    winning_combos_o = ['OOO456789','123OOO789','123456OOO','O23O56O89','1O34O67O9',
-    '12O45O78O','O234O678O','12O4O6O89']
+    winning_combos_x = [/XXX....../,/...XXX.../,/......XXX/,/X..X..X../,/.X..X..X/,/..X..X..X/,/X...X...X/,/..X.X.X../]
+    winning_combos_o = [/OOO....../,/...OOO.../,/......OOO/,/O..O..O../,/.O..O..O/,/..O..O..O/,/O...O...O/,/..O.O.O../]
 
     winning_mark = nil
-    winning_mark = 'X' if winning_combos_x.include?(@board.contents)
-    winning_mark = 'O' if winning_combos_o.include?(@board.contents)  
+    winning_mark = 'X' if winning_combos_x.any?{ |x| x === @board.contents }
+    winning_mark = 'O' if winning_combos_o.any?{ |x| x === @board.contents }  
 
     return @player1.name if @player1.name == winning_mark
     return @player2.name if @player2.name == winning_mark
@@ -60,8 +58,11 @@ class Game
         player = @player1
       end
 
-      puts "Board contents are #{@board.contents}"
-      puts "Winner is #{winner?}"
+      if winner?
+        @board.draw()
+        @status = 0
+      end
+
     end
   end
  
