0c9f982b6c2a15742820cf44958c51fde3070208
Stephen Allard <stephencallard@gmail.com>
2020-05-01 21:49:42 -0500

    Added player_spec.rb

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,7 @@
 require "./lib/player.rb"
 
 describe Player do 
-  context "initialize" do
+  context "#initialize" do
     it "Raises an exception when initialized with an empty {}" do 
       expect{ Player.new({}) }.to raise_error(KeyError)
     end
@@ -10,4 +10,29 @@ describe Player do
       expect{ Player.new({ :name => "john", :mark => "X" }) }.not_to raise_error(KeyError)
     end
   end
+
+
+  context "#name" do 
+    it "Returns name" do
+      player = Player.new( { :name => "john", :mark => "X" })
+      expect(player.name).to eql("john")
+    end
+  end
+
+  context "#mark" do 
+    it "Returns mark" do
+      player = Player.new( { :name => "john", :mark => "X" })
+      expect(player.mark).to eql("X")
+    end
+  end
+
+  context "#move" do
+    it "Returns move" do
+      player = Player.new( { :name => "john", :mark => "X" })
+      player.move = 9
+      expect(player.move).to eql(9)
+    end
+  end
+
+
 end
