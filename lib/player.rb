089a89cf454bcbf01ebe49ecc27b3ba34ba816ab
Stephen Allard <stephencallard@gmail.com>
2020-05-03 03:52:02 -0500

    Removed 'move'

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,9 @@
 class Player
   attr_reader :name, :mark
-  attr_accessor :move
 
   def initialize(args)
     @name = args.fetch(:name)
     @mark = args.fetch(:mark)
-    @move = nil
   end
 
 end
