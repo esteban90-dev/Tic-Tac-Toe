aa6104dbe96d2bf586310c4f5673a74be4f6d78a
Stephen Allard <stephencallard@gmail.com>
2020-05-04 08:28:44 -0500

    Added tests for #result and #switch_players

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,21 +6,44 @@ describe Game do
   let (:joe) { TestPlayer.new({ name: "joe", mark: "X"}) }
   let (:bob) { TestPlayer.new({ name: "bob", mark: "O"}) }
 
-  TestBoard = Struct.new(:contents)
-  let (:board) { TestBoard.new({ contents: "123456789" }) }
-
   context "#initialize" do
     it "Given an array of two players, it sets the first player as current player" do
-      game = Game.new([joe,bob],board)
+      board1 = double('board')
+      game = Game.new([joe,bob],board1)
       expect(game.current_player).to eql(joe)
     end
 
     it "Given an array of two players, it sets the second player as other player" do
-      game = Game.new([joe,bob],board)
+      board1 = double('board')
+      game = Game.new([joe,bob],board1)
       expect(game.other_player).to eql(bob)
     end
+  end
 
-    
+  context "#result" do
+    it "Announces a tie if a there was a tie game" do
+      board1 = double('board')
+      allow(board1).to receive(:tie?) { true }
+      game = Game.new([joe,bob], board1)
+      expect(game.result).to eql("Game was a tie")
+    end
+
+    it "Announces a winner if a there is a winner" do
+      board1 = double('board')
+      allow(board1).to receive(:tie?) { false }
+      game = Game.new([joe,bob], board1)
+      allow(game.current_player).to receive(:name) { "joe" }
+      expect(game.result).to eql("joe is the Winner!")
+    end
+  end
+
+  context "#switch players" do
+    it "switches the current player with the other player" do
+      board1 = double('board')
+      game = Game.new([joe,bob], board1)
+      game.switch_players
+      expect(game.current_player).to eql(bob)
+    end
   end
 
 end
